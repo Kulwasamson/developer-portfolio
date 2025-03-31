c6e3194f83a942f03df61edb02f57c3e1955958a
kulwasamson <kulwasamson010@gmail.comm>
2025-04-01 00:32:47 +0300

    run projecy

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,9 +1,9 @@
 import resume from '../assets/pdf/resume.pdf'
 
 export const headerData = {
-    name: 'Jane Doe',
-    title: "Web Developer",
+    name: 'Kulwa Samson',
+    title: "Full Stack Developer",
     desciption:"There wasn't a bird in the sky, but that was not what caught her attention. It was the clouds. The deep green that isn't the color of clouds, but came with these. She knew what was coming and she hoped she was prepared. ",
-    image: 'https://raw.githubusercontent.com/PhantomScript/asset-container/main/developer-portfolio/landingImg.png',
+    image: 'https://raw.githubusercontent.com/Kulwasamson/Assets-container/d2aef623eae5274e5610fdb82bf61abc1b475599/Ibuprofen1.jpg',
     resumePdf: resume
 }
